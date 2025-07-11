ce2a7759faf84554344b9b6a4669cb4bace2fd91
Prathmesh kulkarni <prathmeshkulkarni312@gmail.com>
2025-07-11 23:28:57 +0530

    yt3

--- frontend/src/components/Hero.tsx
+++ frontend/src/components/Hero.tsx
@@ -1,28 +1,16 @@
-import React from "react";
+const Hero = () => {
+  return (
+    <div
+      className="w-full h-[90vh] bg-cover bg-center relative"
+      style={{ backgroundImage: `url('/image copy.png')` }} // 👈 Put your image here
+    >
+      {/* Dark overlay */}
+      <div className="absolute inset-0 bg-black/60 z-0" />
 
-const Hero: React.FC=()=>{
-return(
-  <div className="relative w-full h-[90vh] overflow-hidden">
-      {/* Outer container for cropping */}
-     <div className="absolute top-0 left-0 w-full h-full overflow-hidden">
- <iframe
-  src="https://www.youtube.com/embed/00FfJa0HwAw?autoplay=1&mute=1&loop=1&playlist=00FfJa0HwAw&controls=0&modestbranding=1&showinfo=0"
-  title="aIDEAS Intro Video"
-  allow="autoplay; encrypted-media"
-  allowFullScreen
-  className="absolute w-[150%] h-[150%] scale-125 -translate-x-[20%] -translate-y-[20%]"
-/>
-
-</div>
-
-
-      {/* Content overlay */}
-      <div className="relative z-10 flex flex-col items-center justify-center h-full text-white bg-black/40">
-        <h1 className="text-4xl md:text-6xl font-bold">Welcome to aIDEAS</h1>
-        <p className="mt-4 text-lg md:text-2xl">Empowering Innovation</p>
-      </div>
+      {/* Text content */}
+      
     </div>
-);
-}
+  );
+};
 
 export default Hero;
