89a65152ce329e53ea1d54c6881e320c3e6577f7
Arjun040 <arjun.ajit704@gmail.com>
2025-07-17 00:30:44 +0530

    tfvh

---
+++ frontend/src/app/members/page.tsx
@@ -0,0 +1,12 @@
+import { JSX } from "react";
+
+const members = (): JSX.Element =>{
+    return(
+        <div>
+        <h1>hello events</h1>
+        </div>
+    )
+
+}
+
+export default members;
